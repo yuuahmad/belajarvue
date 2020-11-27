874e827f4f005db48da0606bd979a00e9bef4404
yusuf <ahmadyusufmaulana0@gmai.com>
2020-11-27 22:32:47 +0700

    versi kedua saya dn sudah saya dply firebase

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <b-button>button</b-button>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-button>button</b-button>
+    <b-button variant="success">Button</b-button>
   </div>
 </template>
